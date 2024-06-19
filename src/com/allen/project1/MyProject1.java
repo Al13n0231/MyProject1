354811c46196b36ce57e2648ec91335cfd5d64f0
Al13n0231 <139857884+Al13n0231@users.noreply.github.com>
2024-06-18 21:21:51 -0400

    solved two sum

--- src/com/allen/project1/MyProject1.java
+++ src/com/allen/project1/MyProject1.java
@@ -40,4 +40,18 @@ public class MyProject1 {
         DogClass myDog = new DogClass("Buddy");
         myDog.bark();
     }
+    public int[] twoSum(int[] num1, int[] num2, int target) {
+    	for (int i = 0; i < num1.length; i++) {
+    		for (int j = i+1; j < num2.length; j++)
+    			if (num1 [i] + num2 [j] == target)
+    				return new int[]{i, j};
+    	}
+    	return null;
+
+    }
 }
+
+// June 17 assignment 2
+// two-sum
+// [1,2,3,4] [5,6,7,8] find two numbers that sum 11
+
